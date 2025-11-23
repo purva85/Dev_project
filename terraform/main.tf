28eecc01fc78542e69bf3a87b9a931f250d7898c
purva12 <143676523+purva85@users.noreply.github.com>
2025-11-23 10:08:21 +0530

    Update key name and tags for AWS instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,9 @@ resource "aws_instance" "my_ec2" {
   ami           = "ami-0a716d3f3b16d290c" 
   instance_type = "t3.micro"
   availability_zone = "eu-north-1a"
-  key_name      = "ipat-eunorth1"
+  key_name      = "ipat-north"
 
   tags = {
-    Name = "firststsproject_1"
+    Name = "purva-firststsproject"
   }
 }
